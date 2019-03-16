3734cbdfa1a53fcb057a2fc0cf3ecd85feda9ba9
boyuangong <astergong@gmail.com>
2019-03-16 01:27:59 -0500

    time interruption

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -3,9 +3,19 @@
 // for a Windows system.
 #![cfg(not(windows))]
 
-use crate::{gdt, println};
+use crate::{gdt, print, println};
 use lazy_static::lazy_static;
 use x86_64::structures::idt::{ExceptionStackFrame, InterruptDescriptorTable};
+use pic8259_simple::ChainedPics;
+use spin;
+
+pub const PIC_1_OFFSET: u8 = 32;
+pub const PIC_2_OFFSET: u8 = PIC_1_OFFSET + 8;
+
+pub const TIMER_INTERRUPT_ID: u8 = PIC_1_OFFSET;
+
+pub static PICS: spin::Mutex<ChainedPics> =
+    spin::Mutex::new(unsafe { ChainedPics::new(PIC_1_OFFSET, PIC_2_OFFSET) });
 
 lazy_static! {
     static ref IDT: InterruptDescriptorTable = {
@@ -16,6 +26,7 @@ lazy_static! {
             idt.double_fault.set_handler_fn(double_fault_handler)
                 .set_stack_index(gdt::DOUBLE_FAULT_IST_INDEX);
         }
+        idt[usize::from(TIMER_INTERRUPT_ID)].set_handler_fn(timer_interrupt_handler);
         idt
     };
 }
@@ -33,5 +44,14 @@ extern "x86-interrupt" fn double_fault_handler(
 {
     println!("EXCEPTION: DOUBLE FAULT\n{:#?}", stack_frame);
     loop {}
+}
+
+extern "x86-interrupt" fn timer_interrupt_handler(_stack_frame: &mut ExceptionStackFrame) {
+    print!(".");
+
+    unsafe {
+        PICS.lock()
+            .notify_end_of_interrupt(TIMER_INTERRUPT_ID);
+    }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -8,20 +8,24 @@ use core::panic::PanicInfo;
 #[cfg(not(test))]
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    use os_rust::interrupts::PICS;
+
     println!("Hello World{}", "!");
 
     os_rust::gdt::init();
     os_rust::interrupts::init_idt();
+    unsafe { PICS.lock().initialize() };
+    x86_64::instructions::interrupts::enable();
 
 //    // invoke a breakpoint exception
 //    x86_64::instructions::int3();
 
-    fn stack_overflow() {
-        stack_overflow(); // for each recursion, the return address is pushed
-    }
-
-    // trigger a stack overflow
-    stack_overflow();
+//    fn stack_overflow() {
+//        stack_overflow(); // for each recursion, the return address is pushed
+//    }
+//
+//    // trigger a stack overflow
+//    stack_overflow();
 
     println!("It did not crush!");
 
